91ea4c7c5dfdcd6ded49a3476925554f868f60c1
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-12-16 01:00:42 +0300

    Changes_return

--- service/main.py
+++ service/main.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-@app.post('/images')
-def file_upload(my_file: UploadFile = File(...)):
-    print(my_file.file.read()) # так можно считать файл
-    return {
-        "image_id": 1
-    }
+@app.post("/items/")
+async def create_item(image: bytes) -> dict:
+    return {"image_id" :  1}
