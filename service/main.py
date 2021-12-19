53bce9faff9e6498d6f858293fc1fabe6bdfda5d
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-12-19 10:19:22 +0300

    Add_detect_faces

--- service/main.py
+++ service/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File
+
+import detect_faces
+
+def count_faces():
+    count_1 = detect_faces.count()
+    return count_1
+
+count_faces()
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(image: bytes) -> dict:
-    return {"image_id" :  1}
+@app.post('/images/')
+async def create_item(image: bytes = File(...)) -> dict:
+    return {"image_id" : 1, "faces" : {count_1}}
--- service/req.py
+++ service/req.py
@@ -1,12 +1,12 @@
 import requests
 
-filename = "E:\projects_python\iStock_87827713_LARGE_9.02.01_AM-1024x640-696x435.jpg"
+filename = 'E:\projects_python\iStock_87827713_LARGE_9.02.01_AM-1024x640-696x435.jpg'
 
 
 url = 'http://127.0.0.1:8000/images'
 
 
-files = {'my_file': (filename, open(filename, 'rb'))}
+files = {'image': (filename, open(filename, 'rb'))}
 
 response = requests.post(url,files=files)
 
