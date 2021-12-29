4f00f9ab3c3d2a4ba235778476e16ca29ebbf113
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-12-29 09:26:17 +0300

    Change_to_alchemy_core
    del, count - не работают

--- service/main.py
+++ service/main.py
@@ -1,18 +1,15 @@
-from fastapi import FastAPI, File, Depends
+from fastapi import FastAPI, File, Depends, HTTPException
 from requests.api import request
 from requests.sessions import Request
 from sqlalchemy.orm import Session
 import uvicorn
-
+#import base64
 from detect_faces import detect
-from write_value import create_image, get_image, del_image
-from count_value import count_image
-from models import Base
-from create_database import SessionLocal, engine
+from write_value import create_image, get_image, del_image, count_image
+from models import *
+#from create_database import SessionLocal, engine
 
 
-
-Base.metadata.create_all(bind=engine) #запускаем базу данных на основе созданной в create_database.py базы данных с примененным видом "таблицы" из models.py
 
 def get_db():
     db = SessionLocal()
@@ -25,31 +22,34 @@ app = FastAPI()
 
 @app.post('/images/')
 async def create_item(image: bytes = File(...), db: Session = Depends(get_db)) -> dict:
-    #теперь create_image делает 2 действия: 
+    
+    #now create_image does 2 actions: 
     faces = detect(image)
     #detect of detect_faces.py
-    db_image = create_image(db, faces=faces)    
+    item = create_image(db, faces=faces)    
     # puts value faces with int in (db: Session, faces: int) in file write_value.py: db_image = models.Image(faces=faces)
     # value faces assign to value model in class Image (models.Image(faces=faces))
     # models.py takes value faces from write_value(class Image) and put faces to value faces of Column in models.py
     
-    return {"image_id" : db_image, "faces" : faces}
-    # теперь image_id возвращает значение из бд, которое генерируется с autoincrement
+    return {"image_id" : item, "faces": faces}
+    # image_id return value from db, generated from autoincrement
 
 @app.get('/images/{image_id}')
 async def get_item(image_id: int, db: Session = Depends(get_db))-> dict:
-    db_image = get_image(db, id=id)
-    return {"image_id" : db_image}
+    db_image = get_image(db, id=image_id)
+    if db_image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    return db_image
 
 @app.delete('/images/{image_id}')
 async def del_item(image_id: int, db: Session = Depends(get_db))-> dict:
-    db_image = del_image(db, id=id)
-    return db_image
+    db_image = del_image(db, id=image_id)
+    return {"id": db_image}
 
-@app.det('/images/count')
-async def del_item(image_id: int, faces: int, time: str, db: Session = Depends(get_db))-> dict:
-    db_image_count = count_image(db, id = id, faces = faces, time = time)
-    pass
+@app.get('/images/count')
+async def count_item(image_id: int, faces: int, time: str, db: Session = Depends(get_db))-> dict:
+    db_image_count = count_image(db, id = image_id, faces = faces, time = time)
+    return {"count": db_image_count}
 
 
 if __name__ == "__main__":
--- service/write_value.py
+++ service/write_value.py
@@ -1,21 +1,65 @@
-from sqlalchemy.orm import Session
-import models
+from sqlalchemy import delete, insert, func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from models import *
+from fastapi import HTTPException
+from detect_faces import detect
+#from create_database import engine
+from sqlalchemy.sql import select
 
-def create_image(db: Session, faces: int):
-    db_image = models.Image(faces=faces)
-    db.add(db_image)
-    db.commit()
-    db.refresh(db_image)
+
+
+def create_image(Session, faces: int):    
+    try:
+        with engine.begin() as conn:            
+            #ins = image_table.insert().values(faces='faces')
+            #ins = conn.execute(image_table.select())
+            result = conn.execute(image_table.insert(),{"faces": faces}).inserted_primary_key 
+                               
+            print(f'В базу добавлено фото c id: {result[0]}')
+    except:
+        Session.rollback()
+        print('ralled back')
+        raise
+    finally:
+        Session.close()
+    return result[0]
+
+
+def get_image (Session, id: int):
+    try:
+        s = select(image_table).where(image_table.c.id == id)
+        with engine.begin() as conn: 
+            for row in conn.execute(s):
+                print(f'Выбрано фото c id: {row[0]}')
+                return row
+                
+    finally:
+        Session.close()
+    #2 with engine.begin() as conn:
+        
+        #result = conn.execute(s)
+        #row = result.fetchone()
+        #return ("id:", row._mapping['id'], "; faces:", row._mapping['faces'], "; datetime:", row._mapping['datetime'])
+    #1  return Session.query(image_table).filter(image_table.id == id).first()
+        
+    
+
+def del_image(Session, id: int):
+    db_image = Session.query(image_table).filter(image_table.id == id).first()
+    if db_image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    Session.delete(db_image)
+    Session.commit()
     return db_image.id
 
-def get_image (db: Session, id: int):
-    db_image = models.Image(id=id)
-    db.query(models.Image).get(id)
-    return db_image.id
+    
 
-def del_image(db: Session, id: int):
-    db_image = models.Image(id=id)
-    db.delete(db_image)
-    db.commit()
-    db.refresh(db_image)
-    return db_image.id
+def count_image(Session, id: int, faces: int, time: str):
+    #images = db.query(Image).filter(Image.id == id, Image.faces == faces, Image.datetime == time).count()
+    #return images
+    try:
+        count_fn = func.count(image_table.c.id == id, image_table.c.faces == faces, image_table.c.datetime == time)
+        return count_fn
+    finally:
+        Session.close()
