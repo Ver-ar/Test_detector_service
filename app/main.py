e680c0cb0521c5fae2c81e4eb39b7d80ebb3c930
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2022-01-11 11:44:56 +0300

    task_loop

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from sqlalchemy.sql import schema
 from detect_faces import detect
 from crud import count_image_faces, create_image, get_image, del_image, get_db, get_notify_users
 from models import *
-import asyncio
 
 from aiogram import Bot, bot, executor
 from aiogram.dispatcher import Dispatcher
@@ -11,27 +10,35 @@ from mytelegrambot.settings import API_KEY
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from mytelegrambot import handlers_
 import logging
+import asyncio
+import concurrent.futures
+
 
 app = FastAPI()
 app.bot = bot
 
+async def main():
+    task1 = loop.create_task(launch_bot())
+    task2 = loop.create_task(create_item())
+    task3 = loop.create_task(count_item_faces())
+    task4 = loop.create_task(get_item())
+    task5 = loop.create_task(get_items())
+    task6 = loop.create_task(del_item())
+    await asyncio.wait([task1, task2,task3,task4,task5,task6])
+
 @app.on_event("startup")
 async def launch_bot():
     bot = Bot(token=API_KEY)
-    #try:
     storage = MemoryStorage()
     dp = Dispatcher(bot=bot, storage=storage)       
     logging.basicConfig(filename='bot.log', format='%(asctime)s-%(message)s', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
-    dp = Dispatcher(bot=bot, storage=storage)       
     handlers_.register_handlers_client(dp)
-    
-    executor.start_polling(dp)
+    dp.start_polling(dp)
+    await asyncio.sleep(0)
+    #await dp.start_polling(dp)
+    #await asyncio.sleep(0)
 
-
-
-    #finally:
-       #await bot.close()
 
 @app.post('/images/')
 async def create_item(image: bytes = File(...)) -> dict:
@@ -40,6 +47,7 @@ async def create_item(image: bytes = File(...)) -> dict:
     item = create_image(faces=faces)   
     get_notify_users(faces) 
     return {"image_id" : item, "faces": faces}
+    
 
 @app.get('/images/count/faces/{faces}')
 async def count_item_faces(faces: int = Path(..., title="The faces on the image to get"))-> dict:
@@ -75,3 +83,11 @@ async def del_item(image_id: int = Path(..., gt=0))-> dict:
         return {"delete image_id": image_id}
 
 
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    except Exception as e:
+        pass
+    finally:
+        loop.close()
