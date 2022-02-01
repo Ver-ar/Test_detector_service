4ff340737f24196c43572139f1176f0516dc41a1
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2022-02-02 01:01:36 +0300

    Update log.py

--- app/log.py
+++ app/log.py
@@ -1,13 +1,19 @@
 import logging
+import sys
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logging.getLogger('uvicorn').setLevel('ERROR')
-logging.getLogger('aiogram').setLevel('ERROR')
-logging.getLogger('sqlalchemy').setLevel('ERROR')
-logging.getLogger('aiosqlite').setLevel('ERROR')
+logger.setLevel(logging.WARNING)
 
+
+logging.getLogger('uvicorn').setLevel('WARNING')
+logging.getLogger('aiogram').setLevel('WARNING')
+logging.getLogger('sqlalchemy').setLevel('WARNING')
+logging.getLogger('aiosqlite').setLevel('WARNING')
+
+sh = logging.StreamHandler(sys.stdout)
 fh = logging.FileHandler('log.log')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
+sh.setFormatter(formatter)
 logger.addHandler(fh)
+logger.addHandler(sh)
