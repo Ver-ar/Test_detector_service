44fe6ac35e24a1a3f42f33f31c2019b44a45d099
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2022-02-02 15:21:24 +0300

    rename_vars

--- app/mytelegrambot/handlers_.py
+++ app/mytelegrambot/handlers_.py
@@ -45,8 +45,8 @@ async def cmd_cancel(message: types.Message, state: FSMContext):
 
 #view
 async def view_all(message: types.Message):
-    photo_list = await crud.get_db()
-    await message.reply("\n".join(f'id: {a[0]}, количество лиц: {a[1]}, дата и время: {a[2]}' for a in photo_list))
+    images_db = await crud.get_db()
+    await message.reply("\n".join(f'id: {item[0]}, количество лиц: {item[1]}, дата и время: {item[2]}' for item in images_db))
 
 #faces
 async def write_value_from_user(message: types.Message):
@@ -58,57 +58,55 @@ async def value_send_func_faces(message: types.Message, state: FSMContext):
         return await message.reply("Введи целое число:")
     else:
         user_id = message.chat.id
-        faces=int(message.text)
-        
+        faces = int(message.text)
     await message.reply(await crud.create_users(faces, user_id))
-
     await state.finish()
 
-async def send_images_faces(message: types.Message):
+async def get_images_faces(message: types.Message):
     await Get.get_faces.set()
     await message.reply('Введи нужное количество лиц на фото:')
 
-async def value_send_func_getface(message: types.Message, state: FSMContext):
+async def value_send_func_get_faces(message: types.Message, state: FSMContext):
     if not message.text.isdigit():
         return await message.reply("Введи целое число:")
     else:
-        faces=int(message.text)
-        photo_list = await crud.get_image_from_faces(faces)
-    if len(photo_list) !=0: 
-        await message.reply("\n".join(f'id: {a[0]}, количество лиц: {a[1]}, дата и время: {a[2]}' for a in photo_list))
+        faces = int(message.text)
+        images_db = await crud.get_image_from_faces(faces)
+    if len(images_db) !=0: 
+        await message.reply("\n".join(f'id: {item[0]}, количество лиц: {item[1]}, дата и время: {item[2]}' for item in images_db))
     else:
         await message.reply(f"Фото с таким количеством лиц не найдено, возможно оно еще не добавлено или удалено")
     await state.finish()         
 
 #getid
-async def send_images_id(message: types.Message):
+async def get_image_id(message: types.Message):
     await GetID.get_id.set()
     await message.reply('Введи интересующий id фото:')
 
-async def value_send_func_getid(message: types.Message, state: FSMContext):
+async def value_send_func_get_id(message: types.Message, state: FSMContext):
     if not message.text.isdigit():
         return await message.reply("Введи целое число:")
     else:
-        id_image=int(message.text)
-        a = await crud.get_image(id_image) 
-        if a is not None:
-            await message.reply(f"id: {a[0]}, количество лиц: {a[1]}")
+        id_image = int(message.text)
+        item = await crud.get_image_with_id(id_image) 
+        if item is not None:
+            await message.reply(f"id: {item[0]}, количество лиц: {item[1]}")
         else:
             await message.reply(f"Фото с таким id не найдено, возможно оно еще не добавлено или удалено")
         await state.finish()
 
 #del
-async def del_images(message: types.Message):    
+async def del_image(message: types.Message):    
     await message.reply('Введи нужный id:')
     await Del.del_id.set()
 
-async def value_send_func_del(message: types.Message, state: FSMContext):
+async def value_send_func_del_id(message: types.Message, state: FSMContext):
     if not message.text.isdigit():
         return await message.reply("Введи целое число:")
     else:
-        id_image=int(message.text)
-        a = await crud.del_image(id_image)    
-        if a is not None: 
+        id_image = int(message.text)
+        item = await crud.del_image(id_image)    
+        if item is not None: 
             await message.reply(f"Удалено фото с id: {id_image}") 
         else: 
             await message.reply(f"Фото с таким id не найдено, возможно оно еще не добавлено или удалено") 
@@ -125,14 +123,14 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(write_value_from_user, commands=['faces'],  state="*")
     dp.register_message_handler(value_send_func_faces, state = Track.faces)
   
-    dp.register_message_handler(send_images_faces, commands=['getface'],  state="*")
-    dp.register_message_handler(value_send_func_getface, state = Get.get_faces)
+    dp.register_message_handler(get_images_faces, commands=['getface'],  state="*")
+    dp.register_message_handler(value_send_func_get_faces, state = Get.get_faces)
 
-    dp.register_message_handler(send_images_id, commands=['getid'], state="*")
-    dp.register_message_handler(value_send_func_getid, state = GetID.get_id)
+    dp.register_message_handler(get_image_id, commands=['getid'], state="*")
+    dp.register_message_handler(value_send_func_get_id, state = GetID.get_id)
     
-    dp.register_message_handler(del_images, commands=['del'], state="*")
-    dp.register_message_handler(value_send_func_del, state = Del.del_id)
+    dp.register_message_handler(del_image, commands=['del'], state="*")
+    dp.register_message_handler(value_send_func_del_id, state = Del.del_id)
 
     dp.register_message_handler(help_menu, commands=['help'])
 
